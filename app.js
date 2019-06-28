20674c228dc63229626a530e0cf04058d7f02e7c
Samer-Laptop <samerstudio@outlook.com>
2019-06-28 10:34:44 -0300

    added router and more info

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 const PORT = 8080
 
+var route_noticias = require('./routes/noticias.js')
+
 app.set('view engine','pug')
 app.use('/static',express.static('static'))
 
@@ -9,6 +11,7 @@ app.get('/',(req,res) => {
 	res.render('landing')
 })
 
+app.use('/noticias', route_noticias)
 
 app.listen(PORT,() => {
 	console.log(`Funcionando en puerto: ${PORT}`)
